8ea18beaf2b7b7264784adeb6b0fa0ae363bdc13
Masao Kitamura <masaok@gmail.com>
2023-07-10 07:23:47 -0700

    Initial Commit

---
+++ Fetch REST API Demo/Fetch_REST_API_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_REST_API_DemoApp.swift
+//  Fetch REST API Demo
+//
+//  Created by keckadmin on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_REST_API_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
